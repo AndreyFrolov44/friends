02fd189cf1914ae89f0a15971e517d3ca4f0e02e
Andreyfrolov44 <andre.frolov44@gmail.com>
2023-05-06 02:37:48 +0300

    related name

--- backend/friends/models.py
+++ backend/friends/models.py
@@ -6,11 +6,11 @@ from users.models import User
 class RequestFriend(models.Model):
     from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='from_user_requests')
     to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='to_user_requests')
-    is_accepted = models.BooleanField(default=False)
+    is_canceled = models.BooleanField(default=False)
 
     class Meta:
         unique_together = ['from_user', 'to_user']
 
 class Friend(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='friends')
-    friends = models.ManyToManyField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='friend')
+    friends = models.ManyToManyField(User, related_name='friends')
