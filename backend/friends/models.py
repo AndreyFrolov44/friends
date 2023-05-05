be125723279dfbd29b0df29bb620433cf8f60e91
Andreyfrolov44 <andre.frolov44@gmail.com>
2023-05-06 00:20:40 +0300

    rename table request to requestfriends

--- backend/friends/models.py
+++ backend/friends/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from users.models import User
 
 
-class Request(models.Model):
+class RequestFriend(models.Model):
     from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='from_user_requests')
     to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='to_user_requests')
     is_canceled = models.BooleanField(default=False)
