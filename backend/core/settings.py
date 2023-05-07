bccd03e87ff7787cef063b8894be162d9e4e228e
Andreyfrolov44 <andre.frolov44@gmail.com>
2023-05-08 00:09:41 +0300

    debug=False

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8(5gr2-vjn%d$8glkw(!skvup2qu(w&jt6es&ph@b4g-=1s@)3'
+SECRET_KEY = getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
