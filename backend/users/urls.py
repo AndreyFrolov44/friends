a5e7c3f5e8bff8b58297317f56351f1561f5e2a9
Andreyfrolov44 <andre.frolov44@gmail.com>
2023-05-08 00:08:37 +0300

    delete user-list and user-me

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from djoser.views import UserViewSet
 
 urlpatterns = [
-    path('users/', UserViewSet.as_view({'get': 'list', 'post': 'create'}), name='user-list'),
-    path('users/me/', UserViewSet.as_view({'get': 'me'}), name='user-me'),
+    path('users/', UserViewSet.as_view({'post': 'create'}), name='user-list'),
 ]
